5ef9b903a97edc96da697562c5a7ffb729046a35
Nikita <fusto_riko1234@mail.ru>
2025-11-15 19:33:27 +0300

    Fix: Add phone field to User model and trust proxy settings

--- models/User.js
+++ models/User.js
@@ -47,6 +47,18 @@ const User = sequelize.define('User', {
   lastLogin: {
     type: DataTypes.DATE,
     allowNull: true
+  },
+  verificationToken: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  resetPasswordToken: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  resetPasswordExpires: {
+    type: DataTypes.DATE,
+    allowNull: true
   }
 }, {
   tableName: 'users',
@@ -57,11 +69,19 @@ const User = sequelize.define('User', {
         const salt = await bcrypt.genSalt(12);
         user.password = await bcrypt.hash(user.password, salt);
       }
+      // Очистка phone если пустая строка
+      if (user.phone === '') {
+        user.phone = null;
+      }
     },
     beforeUpdate: async (user) => {
       if (user.changed('password')) {
         const salt = await bcrypt.genSalt(12);
         user.password = await bcrypt.hash(user.password, salt);
+      }
+      // Очистка phone если пустая строка
+      if (user.changed('phone') && user.phone === '') {
+        user.phone = null;
       }
     }
   }
@@ -77,11 +97,28 @@ User.prototype.isAdmin = function() {
   return this.role === 'admin';
 };
 
+// Get full name
+User.prototype.getFullName = function() {
+  return `${this.firstName} ${this.lastName}`;
+};
+
 // Remove password from JSON output
 User.prototype.toJSON = function() {
   const values = { ...this.get() };
   delete values.password;
+  delete values.verificationToken;
+  delete values.resetPasswordToken;
+  delete values.resetPasswordExpires;
   return values;
+};
+
+// Static methods
+User.findByEmail = function(email) {
+  return this.findOne({ where: { email } });
+};
+
+User.findAdmins = function() {
+  return this.findAll({ where: { role: 'admin' } });
 };
 
 export default User;
